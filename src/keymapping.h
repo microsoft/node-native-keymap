57e68e237f363aef7685d687f4cf5177e0b57f98
Alex Dima <alexdima@microsoft.com>
2015-12-04 10:43:10 +0100

    Add Copyright headers

--- src/keymapping.h
+++ src/keymapping.h
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 #ifndef KEYMAPPING_H_
 #define KEYMAPPING_H_
 
